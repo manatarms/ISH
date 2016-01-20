cb803dd15741d9d8b5c173d974281bf51aaa225f
manatarms <fernandes.sachin@gmail.com>
2016-01-20 12:16:50 -0500

    Third build with splash screen

--- app/src/main/java/indiasarihouse/indiasarihouse/MainActivity.java
+++ app/src/main/java/indiasarihouse/indiasarihouse/MainActivity.java
@@ -1,13 +1,21 @@
 package indiasarihouse.indiasarihouse;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -30,7 +38,22 @@ public class MainActivity extends AppCompatActivity {
         mWebView.setWebViewClient(new WebViewClientDemo());
         mWebView.setWebChromeClient(new WebChromeClientDemo());
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://www.indiasarihouse.com");
+        //Check for connectivity
+        if(isNetworkStatusAvialable(getApplicationContext())) {
+            mWebView.loadUrl("http://www.indiasarihouse.com");
+        } else {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.dialog_title)
+                    .setMessage(R.string.dialog_message)
+                    .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            mWebView.loadUrl("http://www.indiasarihouse.com");
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
         //mWebView.getSettings().setSupportZoom(true);		 //Zoom Control on web (You don't need this
         //if ROM supports Multi-Touch
        // mWebView.getSettings().setBuiltInZoomControls(true); //Enable Multitouch if supported by ROM
@@ -38,12 +61,49 @@ public class MainActivity extends AppCompatActivity {
         // Load URL
     }//onCreate
 
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            if(netInfos != null)
+                if(netInfos.isConnected())
+                    return true;
+        }
+        return false;
+    }
+
 
     private class WebViewClientDemo extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+        //catch no connectivity
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onReceivedError(final WebView view, int errorCode, String description, String failingUrl) {
+            // Handle the error
+
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.dialog_title)
+                    .setMessage(R.string.dialog_message)
+                    .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                           view.reload();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+        @TargetApi(android.os.Build.VERSION_CODES.M)
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+            // Redirect to deprecated method, so you can use it in all SDK versions
+            onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
         }
 
         @Override
